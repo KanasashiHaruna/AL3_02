62dd2b61cc440454731c23b5d8acf2c9f94b0b4a
KanasashiHaruna <146906418+KanasashiHaruna@users.noreply.github.com>
2024-08-04 13:38:43 +0900

    a

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -229,16 +229,16 @@ void Player::Update(
 	//2D標準-----------------------------------------------------------------
 	Vector2 spritePosition = sprite2DReticle_->GetPosition();
 	
-	POINT mousePosition;
+	//POINT mousePosition;
 	//
 	////マウス座標(スクリーン座標)を取得する
-	GetCursorPos(&mousePosition);
+	//GetCursorPos(&mousePosition);
 	////クライアントエリア座標に変換する
-	HWND hwnd = WinApp::GetInstance()->GetHwnd();
-	ScreenToClient(hwnd, &mousePosition);
+	//HWND hwnd = WinApp::GetInstance()->GetHwnd();
+	//ScreenToClient(hwnd, &mousePosition);
 	//
 	////マウス座標を2Dレティクルのスプライトに代入する
-	sprite2DReticle_->SetPosition(Vector2((float)mousePosition.x, (float)mousePosition.y));
+	//sprite2DReticle_->SetPosition(Vector2((float)mousePosition.x, (float)mousePosition.y));
 	//
 	////ビュープロジェクションビューポート合成行列
 	Matrix4x4 matVPV =
